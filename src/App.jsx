ed090f0044bb484453374297690be7c9ef8547f2
algould28 <austin.gould28@gmail.com>
2025-03-19 09:13:26 -0600

    added loading state

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ export default function Book({ initialBookData }) {
 	const [bookData, setBookData] = useState(initialBookData);
 	const [favorites, setFavorites] = useState(undefined);
 	const [loading, setLoading] = useState(true);
+	const [showFavorites, setShowFavorites] = useState(false);
 
 	useEffect(async () => {
 		const ws = new WebSocket(`ws://localhost:9926/GutenBookResult/${bookData.id}`);
@@ -14,6 +15,7 @@ export default function Book({ initialBookData }) {
 			setBookData(message.value);
 		};
 
+		// get the initial favorites data
 		await updateFavorites();
 
 		return () => {
@@ -22,11 +24,13 @@ export default function Book({ initialBookData }) {
 	}, []);
 
 	useEffect(() => {
+		// only display data if everything is ready
 		if (favorites != null && bookData != null) {
 			setLoading(false);
 		}
 	}, [favorites, bookData]);
 
+	//wanted to use a websocket here for real time updates but could not figure out how to append the consecutive streams
 	const updateFavorites = useCallback(async () => {
 		console.log('UPDATE FAVS');
 		const favoritesResponse = await fetch('http://localhost:9926/GetFavorites/');
@@ -37,27 +41,45 @@ export default function Book({ initialBookData }) {
 	return (
 		<article style={{ padding: '1rem 2rem' }}>
 			<div style={{ color: '#FDFDFD', fontSize: '2rem', fontWeight: 'bold', marginBottom: '0.5rem' }}>Book Results</div>
-			<div
-				style={{
-					display: 'grid',
-					gridTemplateColumns: 'repeat(4, 1fr)',
-					gap: '1rem',
-					marginBottom: '1rem',
-					maxHeight: '85vh',
-					overflowY: 'scroll',
-					scrollbarColor: '#FFFFFF00 #FFFFFF00',
-					padding: '0.5rem',
-				}}
-			>
-				{!loading && favorites != null && bookData != null ? (
-					bookData.results.map((book) => {
-						const isFavorite = favorites.some((favoriteData) => `${book.id}` === `${favoriteData.id}`);
-						return <BookCard book={book} isFavorite={isFavorite} updateFavorites={updateFavorites} />;
-					})
-				) : (
-					<div>loading</div>
-				)}
-			</div>
+			{!loading && favorites != null && bookData != null ? (
+				<div
+					style={{
+						display: 'grid',
+						gridTemplateColumns: 'repeat(4, 1fr)',
+						gap: '1rem',
+						marginBottom: '1rem',
+						maxHeight: '85vh',
+						overflowY: 'scroll',
+						scrollbarColor: '#FFFFFF00 #FFFFFF00',
+						padding: '0.5rem',
+					}}
+				>
+					{showFavorites
+						? favorites.results.map((favorite) => {
+								return <BookCard book={favorite} isFavorite={true} updateFavorites={updateFavorites} />;
+							})
+						: bookData.results.map((book) => {
+								const isFavorite = favorites.some((favoriteData) => `${book.id}` === `${favoriteData.id}`);
+								return <BookCard book={book} isFavorite={isFavorite} updateFavorites={updateFavorites} />;
+							})}
+				</div>
+			) : (
+				// Ideally would have skeleton loading here
+				<div
+					style={{
+						display: 'flex',
+						alignItems: 'center',
+						justifyContent: 'center',
+						width: '100%',
+						height: '88vh',
+						color: '#FDFDFD',
+						fontSize: '4rem',
+						fontWeight: 'bold',
+					}}
+				>
+					Loading...
+				</div>
+			)}
 
 			<Pagination bookData={bookData} />
 		</article>
